da11ad90d41472926e878e823b1ef594655c8b4f
Spencer Russell <spencer.f.russell@gmail.com>
2019-02-15 15:47:28 -0500

    starting to add more systematic composition tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,13 +3,44 @@ using ToyAD: CtoR, NonHolomorphic, AntiHolomorphic
 using ToyAD: wirtprimal, wirtconj
 using Test
 
-# define a nonholomorphic function and its derivatives.
-nonholo(z) = 3z^2 * 5conj(z)^3
-dnonholodz(z) = 6z * 5conj(z)^3
-dnonholodz̄(z) = 3z^2 * 15conj(z)^2
-# define an explicit diff rule so we're not testing any function composition
-@partials nonholo(z) = (NonHolomorphic(dnonholodz(z), dnonholodz̄(z)),)
+# define some known functions that we can use to test function composition. We
+# define partials for these so that they themselves aren't made of composed
+# functions
+@noinline holo(z) = z^2
+dholo_dz(z) = 2z
+dholo_dz̄(z) = 0
+@partials holo(z) = (dholo_dz(z),)
+@add_forward_unary holo
+
+@noinline nonholo(z) = 3z^2 * 5conj(z)^3
+dnonholo_dz(z) = 6z * 5conj(z)^3
+dnonholo_dz̄(z) = 3z^2 * 15conj(z)^2
+@partials nonholo(z) = (NonHolomorphic(dnonholo_dz(z), dnonholo_dz̄(z)),)
 @add_forward_unary nonholo
+
+@noinline antiholo(z) = conj(z^2)
+dantiholo_dz(z) = 0
+dantiholo_dz̄(z) = 2*conj(z)
+@partials antiholo(z) = (AntiHolomorphic(dantiholo_dz̄(z)),)
+@add_forward_unary antiholo
+
+@noinline ctor(z) = conj(z)*z
+dctor_dz(z) = conj(z)
+dctor_dz̄(z) = z
+@partials ctor(z) = (CtoR(dctor_dz(z)),)
+@add_forward_unary ctor
+
+# test a dual function
+function testforward(fn, expectedT, dfdz=nothing, dfdz̄=nothing)
+    z = 10.0+1.0im
+    dualz = dualseed(z)
+    dualf = fn(dualz)
+    df = partials(dualf)[1]
+    @test value(dualf) ≈ fn(z)
+    @test df isa expectedT
+    @test wirtprimal(df) ≈ dfdz(z)
+    dfdz̄ !== nothing && @test wirtconj(df) ≈ dfdz̄(z)
+end
 
 @testset begin
 @testset "basic arithmetic" begin
@@ -43,13 +74,7 @@ end
 end
 
 @testset "CtoR" begin
-    z = 10.0+1.0im
-    dualz = dualseed(z)
-    dualf = abs2(dualz)
-    dfdz = partials(dualf)[1]
-    @test value(dualf) ≈ abs2(z)
-    @test dfdz isa CtoR
-    @test wirtprimal(dfdz) ≈ conj(z)
+    testforward(abs2, CtoR, conj)
 end
 
 @testset "AntiHolomorphic" begin
@@ -75,7 +100,31 @@ end
     df = partials(dualf)[1]
     @test value(dualf) ≈ nonholo(z)
     @test df isa NonHolomorphic
-    @test wirtprimal(df) ≈ dnonholodz(z)
-    @test wirtconj(df) ≈ conj(dnonholodz̄(z))
+    @test wirtprimal(df) ≈ dnonholo_dz(z)
+    @test wirtconj(df) ≈ conj(dnonholo_dz̄(z))
 end
+
+# test all compositions of function types
+# @testset "Composition Matrix" begin
+#     testforward(z->nonholo(nonholo(z)), NonHolomorphic, , )
+#     testforward(z->nonholo(holo(z)), NonHolomorphic, , )
+#     testforward(z->nonholo(antiholo(z)), NonHolomorphic, , )
+#     testforward(z->nonholo(ctor(z)), NonHolomorphic, , )
+#
+#     testforward(z->holo(nonholo(z)), NonHolomorphic, , )
+#     testforward(z->holo(holo(z)), Complex, 4z^3, 0)
+#     testforward(z->holo(antiholo(z)), AntiHolomorphic, , )
+#     testforward(z->holo(ctor(z)), NonHolomorphic, , )
+#
+#     testforward(z->antiholo(nonholo(z)), NonHolomorphic, , )
+#     testforward(z->antiholo(holo(z)), AntiHolomorphic, , )
+#     testforward(z->antiholo(antiholo(z)), Complex, , )
+#     testforward(z->antiholo(ctor(z)), NonHolomorphic, , )
+#
+#     testforward(z->ctor(nonholo(z)), CtoR, , )
+#     testforward(z->ctor(holo(z)), CtoR, , )
+#     testforward(z->ctor(antiholo(z)), CtoR, , )
+#     testforward(z->ctor(ctor(z)), CtoR, , )
+# end
+
 end
